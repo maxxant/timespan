9a6ad351cc8f07f33013788b2c521c09a28366bc
maxxant <maxxant@gmail.com>
2024-01-10 13:57:40 +0300

    make TimeMax TimeMin public. Fix comments

--- timespan.go
+++ timespan.go
@@ -1,4 +1,4 @@
-//Package timespan provides functionality for handling intervals of time.
+// Package timespan provides functionality for handling intervals of time.
 package timespan
 
 import (
@@ -8,11 +8,11 @@ import (
 	"time"
 )
 
-//Span represents an inclusive range between two time instants.
+// Span represents an inclusive range between two time instants.
 //
-//The zero value of type span has both start and end times set to the zero value
-//of type Time. The zero value is returned by the Intersection and Gap methods
-//when there is no span fitting their purposes.
+// The zero value of type span has both start and end times set to the zero value
+// of type Time. The zero value is returned by the Intersection and Gap methods
+// when there is no span fitting their purposes.
 type Span struct {
 	start, end time.Time
 }
@@ -59,7 +59,7 @@ func (s *Span) UnmarshalJSON(data []byte) error {
 
 }
 
-//New
+// NewTimes creates a new span with the given start and end instants.
 func NewTimes(t1, t2 time.Time) Span {
 	start := t1
 	end := t2
@@ -73,7 +73,7 @@ func NewTimes(t1, t2 time.Time) Span {
 	}
 }
 
-//New creates a new span with the given start instant and duration.
+// New creates a new span with the given start instant and duration.
 func New(t time.Time, d time.Duration) Span {
 	start := t
 	end := t.Add(d)
@@ -87,98 +87,98 @@ func New(t time.Time, d time.Duration) Span {
 	}
 }
 
-//Start returns the time instant at the start of s.
+// Start returns the time instant at the start of s.
 func (s Span) Start() time.Time {
 	return s.start
 }
 
-//End returns the time instant at the end of s.
+// End returns the time instant at the end of s.
 func (s Span) End() time.Time {
 	return s.end
 }
 
-//Duration returns the length of time represented by s.
+// Duration returns the length of time represented by s.
 func (s Span) Duration() time.Duration {
 	return s.end.Sub(s.start)
 }
 
-//After reports whether s begins after t.
+// After reports whether s begins after t.
 func (s Span) After(t time.Time) bool {
 	return s.start.After(t)
 }
 
-//Before reports whether s ends before t.
+// Before reports whether s ends before t.
 func (s Span) Before(t time.Time) bool {
 	return s.end.Before(t)
 }
 
-//Borders reports whether s and r are contiguous time intervals.
+// Borders reports whether s and r are contiguous time intervals.
 func (s Span) Borders(r Span) bool {
 	return s.start.Equal(r.end) || s.end.Equal(r.start)
 }
 
-//ContainsTime reports whether t is within s.
+// ContainsTime reports whether t is within s.
 func (s Span) ContainsTime(t time.Time) bool {
 	return !(t.Before(s.start) || t.After(s.end))
 }
 
-//Contains reports whether r is entirely within s.
+// Contains reports whether r is entirely within s.
 func (s Span) Contains(r Span) bool {
 	return s.ContainsTime(r.start) && s.ContainsTime(r.end)
 }
 
-//Encompass returns the minimum span that fully contains both r and s.
+// Encompass returns the minimum span that fully contains both r and s.
 func (s Span) Encompass(r Span) Span {
 	return Span{
-		start: tmin(s.start, r.start),
-		end:   tmax(s.end, r.end),
+		start: TimeMin(s.start, r.start),
+		end:   TimeMax(s.end, r.end),
 	}
 }
 
-//Equal reports whether s and r represent the same time intervals, ignoring
-//the locations of the times.
+// Equal reports whether s and r represent the same time intervals, ignoring
+// the locations of the times.
 func (s Span) Equal(r Span) bool {
 	return s.start.Equal(r.start) && s.end.Equal(r.end)
 }
 
-//Follows reports whether s begins after or at the end of r.
+// Follows reports whether s begins after or at the end of r.
 func (s Span) Follows(r Span) bool {
 	return !s.start.Before(r.end)
 }
 
-//Gap returns a span corresponding to the period between s and r.
-//If s and r have a non-zero overlap, a zero span is returned.
+// Gap returns a span corresponding to the period between s and r.
+// If s and r have a non-zero overlap, a zero span is returned.
 func (s Span) Gap(r Span) Span {
 	if s.Overlaps(r) {
 		return Span{}
 	}
 	return Span{
-		start: tmin(s.end, r.end),
-		end:   tmax(s.start, r.start),
+		start: TimeMin(s.end, r.end),
+		end:   TimeMax(s.start, r.start),
 	}
 }
 
-//Intersection returns both a span corresponding to the non-zero overlap of
-//s and r and a bool indicating whether such an overlap existed.
-//If s and r do not overlap, a zero span is returned with false.
+// Intersection returns both a span corresponding to the non-zero overlap of
+// s and r and a bool indicating whether such an overlap existed.
+// If s and r do not overlap, a zero span is returned with false.
 func (s Span) Intersection(r Span) (Span, bool) {
 	if !s.Overlaps(r) {
 		return Span{}, false
 	}
 	return Span{
-		start: tmax(s.start, r.start),
-		end:   tmin(s.end, r.end),
+		start: TimeMax(s.start, r.start),
+		end:   TimeMin(s.end, r.end),
 	}, true
 }
 
-//IsZero reports whether s represents the zero-length span starting and ending
-//on January 1, year 1, 00:00:00 UTC.
+// IsZero reports whether s represents the zero-length span starting and ending
+// on January 1, year 1, 00:00:00 UTC.
 func (s Span) IsZero() bool {
 	return s.start.IsZero() && s.end.IsZero()
 }
 
-//Offset returns s with its start time offset by d. It is equivalent to
-//Newspan(s.Start().Add(d), s.Duration()).
+// Offset returns s with its start time offset by d. It is equivalent to
+// New(s.Start().Add(d), s.Duration()).
 func (s Span) Offset(d time.Duration) Span {
 	return Span{
 		start: s.start.Add(d),
@@ -186,9 +186,9 @@ func (s Span) Offset(d time.Duration) Span {
 	}
 }
 
-//OffsetDate returns s with its start time offset by the given years, months,
-//and days. It is equivalent to
-//Newspan(s.Start().AddDate(years, months, days), s.Duration()).
+// OffsetDate returns s with its start time offset by the given years, months,
+// and days. It is equivalent to
+// New(s.Start().AddDate(years, months, days), s.Duration()).
 func (s Span) OffsetDate(years, months, days int) Span {
 	d := s.Duration()
 	t := s.start.AddDate(years, months, days)
@@ -198,26 +198,26 @@ func (s Span) OffsetDate(years, months, days int) Span {
 	}
 }
 
-//Overlaps reports whether s and r intersect for a non-zero duration.
+// Overlaps reports whether s and r intersect for a non-zero duration.
 func (s Span) Overlaps(r Span) bool {
 	return s.start.Before(r.end) && s.end.After(r.start)
 }
 
-//Precedes reports whether s ends before or at the start of r.
+// Precedes reports whether s ends before or at the start of r.
 func (s Span) Precedes(r Span) bool {
 	return !s.end.After(r.start)
 }
 
-//tmax returns the later of two time instants.
-func tmax(t, u time.Time) time.Time {
+// TimeMax returns the later of two time instants.
+func TimeMax(t, u time.Time) time.Time {
 	if t.After(u) {
 		return t
 	}
 	return u
 }
 
-//tmin returns the earlier of two time instants.
-func tmin(t, u time.Time) time.Time {
+// TimeMin returns the earlier of two time instants.
+func TimeMin(t, u time.Time) time.Time {
 	if t.Before(u) {
 		return t
 	}
